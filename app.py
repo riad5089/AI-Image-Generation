405a5da1c8c780a56e388b69b62bd55bf28de829
Md Riad <riad5089@gmail.com>
2024-03-04 00:09:30 +0600

    udpated

---
+++ app.py
@@ -0,0 +1,39 @@
+
+import openai
+import urllib.request
+from PIL import Image
+import streamlit as st
+
+
+
+openai.api_key = "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"
+
+
+def generate_image(image_description):
+
+  img_response = openai.Image.create(
+    prompt = image_description,
+    n=1,
+    size="512x512")
+  
+
+  img_url = img_response['data'][0]['url']
+
+  urllib.request.urlretrieve(img_url, 'img.png')
+
+  img = Image.open("img.png")
+  
+  return img
+
+
+
+# page title
+st.title('DALL.E - Image Generation - OpenAI')
+
+# text input box for image recognition
+img_description = st.text_input('Image Desription')
+
+if st.button('Generate Image'):
+    
+    generated_img = generate_image(img_description)
+    st.image(generated_img)
